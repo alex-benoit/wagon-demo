ab641a566103e767ef25da60fe2175854fef4410
Alex Benoit <alexander.benoit@outlook.com>
2017-05-26 09:40:31 +0100

    added photo field to products

--- Gemfile
+++ Gemfile
@@ -26,7 +26,6 @@ gem 'cloudinary'
 gem 'carrierwave', '~> 0.11.2'
 
 
-
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170526083702) do
+ActiveRecord::Schema.define(version: 20170526084007) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20170526083702) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "photo"
   end
 
 end
